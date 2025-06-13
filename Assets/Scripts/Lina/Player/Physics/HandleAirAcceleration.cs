ac6a7d0197bb2b82cc422b8d2349ee0634b10faf
Pukki <volentie@gmail.com>
2025-06-13 15:46:35 -0300

    Add quake style air acceleration

--- Assets/Scripts/Lina/Player/Physics/HandleAirAcceleration.cs
+++ Assets/Scripts/Lina/Player/Physics/HandleAirAcceleration.cs
@@ -2,16 +2,32 @@ using UnityEngine;
 
 namespace Lina.Player.Physics
 {
-    class HandleAirAcceleration : MonoBehaviour, IHandleAirAcceleration
-    {
+	class HandleAirAcceleration : MonoBehaviour, IHandleAirAcceleration
+	{
 		public float AirAcceleration => 1.5f;
 		public void AirAccelerate(Vector3 wishDir, ref Vector3 velocity, Transform transform)
 		{
-			Vector3 forwardDir = transform.forward;
-			float projSpeed = Vector3.Dot(wishDir.normalized, forwardDir);
-			Vector3 sumForward = new Vector3(forwardDir.x * projSpeed, 0, forwardDir.z * projSpeed);
-			sumForward *= AirAcceleration;
-			velocity += sumForward * Time.deltaTime;
+			// Turn wishDir (local input) into a world‐space direction vector
+			Vector3 worldWish = (transform.forward * wishDir.z + transform.right * wishDir.x).normalized;
+
+			// How fast we’d like to move along that direction
+			float wishSpeed = wishDir.magnitude * AirAcceleration;
+
+			// How fast we’re already moving along that direction
+			float currentSpeed = Vector3.Dot(velocity, worldWish);
+
+			// Speed we need to add
+			float addSpeed = wishSpeed - currentSpeed;
+			if (addSpeed <= 0f)
+				return;
+
+			// How much acceleration we can apply this frame
+			float accelSpeed = AirAcceleration * Time.deltaTime * wishSpeed;
+			if (accelSpeed > addSpeed)
+				accelSpeed = addSpeed;
+
+			// Finally, accelerate
+			velocity += worldWish * accelSpeed;
 		}
-    }
+	}
 }
